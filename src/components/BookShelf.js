1ef3f19c2f134714a41e7b2f9be5dd0170946cab
Hlyan Htet <zawhlyan.htet@wavemoney.com.mm>
2021-07-15 20:00:20 +0630

    feat: refactor to apply router and breakdown components

---
+++ src/components/BookShelf.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Book from './Book';
+
+class BookShelf extends Component {
+    render() {
+        return (
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">Shelf Title</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        <li>
+                            <Book/>
+                        </li>
+                    </ol>
+                </div>
+            </div>     
+        );
+    }
+}
+
+export default BookShelf;
---
+++ src/pages/MyBooks.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import BookShelf from './../components/BookShelf'
+import { Link } from 'react-router-dom'
+
+class MyBooks extends Component {
+    render() {
+        return (
+            <div className="list-books">
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    <BookShelf/>
+                </div>
+                <div className="open-search">
+                    <Link to='/search'>Add a book</Link>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default MyBooks;
